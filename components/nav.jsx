2f0574bb81951b97917e9333147048f79b1a17fd
samthambad <thambad@gmail.com>
2024-04-06 14:23:47 +0800

    login api route in nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,29 +2,32 @@ import React from 'react'
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from 'react'
 const Nav = () => {
-    const router = useRouter()
-    async function signInWithLinkedIn() {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-            provider: 'linkedin_oidc',
-            options: {
-                redirectTo: location.origin + "/"
-            }
-        })
+  const router = useRouter()
+  const [loginData, setLoginData] = useState()
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  async function signInWithLinkedIn() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'linkedin_oidc',
+      options: {
+        redirectTo: location.origin + "/"
+      }
+    })
+  }
+  async function signOutFromLinkedin() {
+    await supabase.auth.signOut()
+    setIsLoggedIn(false)
+    router.push("/");
+    router.refresh();
+  }
+  useEffect(() => {
+    const getLoginData = async () => {
+      const { data } = await supabase.auth.getSession();
+      setLoginData(data)
     }
-    async function signOutFromLinkedin() {
-        await supabase.auth.signOut()
-        setIsLoggedIn(false)
-        router.push("/");
-        router.refresh();
-    }
-    useEffect(() => {
-        const getLoginData = async () => {
-            const { data } = await supabase.auth.getSession();
-        }
-    }, [])
-    return (
-        <div>Nav</div>
-    )
+  }, [])
+  return (
+    <div>Nav</div>
+  )
 }
 
 export default Nav
