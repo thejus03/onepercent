6b9e4da51becc5a8811e014c0866dddafb56a5e5
samthambad <thambad@gmail.com>
2024-04-06 17:06:44 +0800

    displaying search

--- components/searchbar.jsx
+++ components/searchbar.jsx
@@ -12,6 +12,18 @@ const SearchComponent = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname();
 
+  const handleRedirect = (redirecturl, redirectsection, event) => {
+    const params = new URLSearchParams(searchParams)
+    params.set("animate", redirectsection)
+    replace(`${redirecturl == 'network' && 'network'}?${params.toString()}`)
+    setTimeout(() => {
+      const sectionElement = document.getElementById(redirectsection)
+      if (sectionElement) {
+        sectionElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 1000)
+
+  }
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams)
     if (term) {
@@ -25,6 +37,21 @@ const SearchComponent = () => {
     replace(`${pathname}?${params.toString()}`)
   }, 600)
 
+  useEffect(() => {
+    const getResults = async (queryTerm) => {
+      try {
+        const results = await universalSearch(queryTerm)
+        setQueryResults(results)
+        console.log(results[0].html_id)
+      }
+      catch (error) {
+        console.log("error fetching search results", error)
+      }
+    }
+    getResults(query)
+
+  }, [query])
+
   return (
     <div className='relative flex flex-col'>
       <input
@@ -36,6 +63,16 @@ const SearchComponent = () => {
         type="search"
       />
       {/* display search */}
+      <ul className={`absolute left-0 top-8 w-full mt-2 border-gray-300 rounded shadow-md z-10 ${queryResults?.length && 'bg-white border'}`}>
+        {queryResults?.length > 0 && queryResults.map((result, i) => (
+          <li onClick={() => handleRedirect(result.tag, result.html_id, event)} className='py-2 px-4 hover:bg-gray-100 border-y-[1px] border-grey-100' key={i}>
+            <div className='max-w-full'>
+              <h4 className='font-bold'>{result.title}</h4>
+              <span className='flex flex-wrap max-w-full whitespace-normal text-wrap break-all'>{result.description}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
--- utils/supabase/actions.js
+++ utils/supabase/actions.js
@@ -28,6 +28,48 @@ export async function checkUser(user_id) {
   return false
 }
 
+export async function universalSearch(query) {
+  if (query.length === 0) return
+  const tags = await getAllTags()
+  if (tags.includes(query)) {
+    const { data: networkData, error: networkError } = await supabase
+      .from('network')
+      .select('html_id,title,description,tags')
+    const filteredNetworkData = networkData.filter(data => data?.tags?.includes(query))
+    const networkDataWithTag = filteredNetworkData?.map(item => ({ ...item, tag: 'network' }));
+    return networkDataWithTag
+
+  }
+  const { data: networkData, error: networkError } = await supabase
+    .from('network')
+    .select('html_id,title,description')
+    .or(`title.ilike.%${query}%,description.ilike.%${query}%`)
+    .limit(6)
+
+  if (networkError) {
+    console.error('Error fetching network data:', networkError.message);
+    return [];
+  }
+
+  const { data: serviceData, error: serviceError } = await supabase
+    .from('service')
+    .select('html_id,title,description')
+    .or(`title.ilike.%${query}%,description.ilike.%${query}%`)
+    .limit(6)
+  if (serviceError) {
+    console.error('Error fetching service data:', serviceError.message);
+    return [];
+  }
+
+  // Combine the rows/results from both tables
+  const networkDataWithTag = networkData.map(item => ({ ...item, tag: 'network' }));
+  const serviceDataWithTag = serviceData.map(item => ({ ...item, tag: 'service' }));
+
+  const combinedData = [...networkDataWithTag, ...serviceDataWithTag];
+
+  return combinedData;
+}
+
 // FOR chatbot
 export async function getChats(user_id) {
   let { data, error } = await supabase.from("chats").select().match({ id: user_id })
